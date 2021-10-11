4f67fa6088c3aa8884bb5bc177f64773f6e335b5
a.borgatin <a.borgatin@corp.vk.com>
2021-10-11 17:21:14 +0300

    family tree some data

--- tree/famdata.js
+++ tree/famdata.js
@@ -1,11 +1,12 @@
 ﻿var famdata = [
     {
+        parents: [4, 5],
         id: 1,
         title: "Боргатин Александр Васильевич",
         label: "Боргатин Александр Васильевич",
         description: "Оренбург/Санкт-Петербург, ИТ-специалист",
         email: "aborgatin@yandex.ru",
-        image: "photos/e.png",
+        image: "photos/b.png",
         itemTitleColor: "#c8e4fb"
     },
     {
@@ -14,7 +15,7 @@
         label: "Байназарова Нурзиля Мухаметовна",
         description: "Башкирия/Санкт-Петербург",
         email: "nurzilyasha@gmail.com",
-        image: "photos/e.png",
+        image: "photos/t.png",
         itemTitleColor: "#c8e4fb"
     },
     {
@@ -23,7 +24,57 @@
         title: "Боргатина Вероника Александровна",
         label: "Боргатина Вероника Александровна",
         description: "",
-        image: "photos/e.png",
+        image: "photos/f.png",
+        itemTitleColor: "#c8e4fb"
+    },
+    {
+        id: 4,
+        title: "Боргатина (Пивцаева) Надежда Николаевна",
+        label: "Боргатина (Пивцаева) Надежда Николаевна",
+        description: "Александровка, Оренбургская область, мед. сестра",
+        image: "photos/p.png",
+        itemTitleColor: "#c8e4fb"
+    },
+    {
+        parents: [6, 7],
+        id: 5,
+        title: "Боргатин Василий Александрович",
+        label: "Боргатин Василий Александрович",
+        description: "Александровка, Оренбургская область",
+        image: "photos/father.jpg",
+        itemTitleColor: "#c8e4fb"
+    },
+    {
+        parents: [8, 9],
+        id: 6,
+        title: "Боргатин Александр Федорович",
+        label: "Боргатин Александр Федорович",
+        description: "1937 - 2009",
+        image: "photos/b.png",
+        itemTitleColor: "#c8e4fb"
+    },
+    {
+        id: 7,
+        title: "Боргатина (Кузьмина) Нина Павловна",
+        label: "Боргатина (Кузьмина) Нина Павловна",
+        description: "1939 - 1997",
+        image: "photos/p.png",
+        itemTitleColor: "#c8e4fb"
+    },
+    {
+        id: 8,
+        title: "Боргатин Федор Петрович",
+        label: "Боргатин Федор Петрович",
+        description: "1905 - 1970",
+        image: "photos/b.png",
+        itemTitleColor: "#c8e4fb"
+    },
+    {
+        id: 9,
+        title: "Боргатина (Сынкова) Пелагея Никитична",
+        label: "Боргатина (Сынкова) Пелагея Никитична",
+        description: "1906 - 19**",
+        image: "photos/p.png",
         itemTitleColor: "#c8e4fb"
     },
 ];
